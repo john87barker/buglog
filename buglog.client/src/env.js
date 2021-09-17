834af13de8a15de43fa4dfd55edda2b903dea218
john87barker <john87barker@gmail.com>
2021-09-17 15:47:59 -0600

    trying to build and launch

--- buglog.client/src/env.js
+++ buglog.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const domain = 'john87barker.us.auth0.com'
-export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
+export const clientId = 'QDEPzwUGYLPDpbduros4CY73LftMWiqb'
 export const audience = 'https://buglog1.herokuapp.com/#/'
